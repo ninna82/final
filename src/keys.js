e4bfdc2f51f17da792421a1a2db257b81794e926
ninna82 <65990945+ninna82@users.noreply.github.com>
2020-05-26 21:21:25 -0500

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+  
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'bd_links'
+    }
+
+};
